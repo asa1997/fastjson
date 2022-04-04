531048415c378f0b1252cdff0a83b10138f0e7e9
asa1997 <arunsureshampadath@gmail.com>
2022-04-04 14:45:00 +0530

    Commented unused package

--- src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest.java
+++ src/test/java/com/alibaba/json/bvt/serializer/DoubleFormatTest.java
@@ -2,7 +2,7 @@ package com.alibaba.json.bvt.serializer;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.annotation.JSONField;
-import com.sun.javafx.sg.prism.NGShape;
+// import com.sun.javafx.sg.prism.NGShape;
 import junit.framework.TestCase;
 
 import java.text.DecimalFormat;
