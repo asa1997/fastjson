1319036d0fdeea0fed95e5d61629c9b3044e480f
asa1997 <arunsureshampadath@gmail.com>
2022-04-08 17:36:18 +0530

    Updated WriteClassNameTest_Sets

--- src/test/java/com/alibaba/json/bvt/writeClassName/WriteClassNameTest_Set.java
+++ src/test/java/com/alibaba/json/bvt/writeClassName/WriteClassNameTest_Set.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import com.alibaba.fastjson.parser.ParserConfig;
 import org.junit.Assert;
 import junit.framework.TestCase;
 
@@ -11,6 +12,9 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public class WriteClassNameTest_Set extends TestCase {
+    protected void setUp() throws Exception {
+        ParserConfig.global.addAccept("com.alibaba.json.bvt.writeClassName.WriteClassNameTest_Set");
+    }
 
     public void test_list() throws Exception {
         A a = new A();
@@ -20,7 +24,7 @@ public class WriteClassNameTest_Set extends TestCase {
         a.setList(set);
         String text = JSON.toJSONString(a, SerializerFeature.WriteClassName);
         System.out.println(text);
-        Assert.assertEquals("{\"@type\":\"com.alibaba.json.bvt.writeClassName.WriteClassNameTest_Set$A\",\"list\":[{},{\"@type\":\"com.alibaba.json.bvt.writeClassName.WriteClassNameTest_Set$B1\"}]}",
+        Assert.assertEquals("{\"@type\":\"com.alibaba.json.bvt.writeClassName.WriteClassNameTest_Set$A\",\"list\":Set[{},{\"@type\":\"com.alibaba.json.bvt.writeClassName.WriteClassNameTest_Set$B1\"}]}",
                             text);
 
         A a1 = (A) JSON.parse(text);
